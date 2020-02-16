609e5e97bea6640308a41c14d722a9ede6be8092
viewbook <lionlinux2018@gmail.com>
2020-02-16 19:55:53 +0800

    Build: 组件的核心概念-插槽

---
+++ 基础篇/vue组件核心概念/3.Slot.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-02-16 19:42:32
+ * @LastEditTime: 2020-02-16 19:42:38
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue\course-of-vue\基础篇\vue组件核心概念\3.Slot.vue
+ -->
+<template>
+    <div>
+        <slot />
+        <slot name="title"/>
+        <slot name="item" v-bind="{value:'vue'}">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SlotDemo'
+}
+</script>
+
+<style>
+
+</style>
--- 基础篇/vue组件核心概念/index.vue
+++ 基础篇/vue组件核心概念/index.vue
@@ -9,7 +9,7 @@
 <template>
     <div id="app">
         <a-tabs>
-            <a-tab-pane>
+            <a-tab-pane key="props" tab="属性">
                 <!-- 使用组件传递属性值 -->
                 <!-- :on-change是专门修改属性的 -->
                 <Props  
@@ -26,12 +26,37 @@
                 <!-- 课后习题 -->
                 <!--  子组件为何不可以修改父组件传递的Prop,如果修改了,Vue是如何监控到属性的修改并给出警告的? -->
             </a-tab-pane>
+            <a-tab-pane key="event" tab="事件"> 
+                <Event :name="name" @change="handleEventChange">
+            </a-tab-pane>
+            <a-tab-pane  key="slot" tab="插槽">
+                <h2>2.6新语法</h2>
+                <SlotDemo>
+                    <p>default slot</p>
+                    <template v-slot:title>
+                        <p>title slot1</p>
+                        <p>title slot2</p>
+                    </template>
+                    <template v-slot:item="props">
+                        <p>item slot-scope {{props}}</p>
+                    </template>
+                </SlotDemo>
+                 <h2>2.5老语法</h2>
+                <SlotDemo>
+                    <p>default slot</p>
+                    <p slot="title">title slot1</p>
+                    <p slot="title">title slot2</p>
+                    <p slot="item" slot-scope="props">item slot-scope {{props}}</p>
+                </SlotDemo>
+            </a-tab-pane>
         </a-tabs>
     </div>
 </template>
 
 <script>
 import Props from '1.Props.vue';
+import Event from '2.Event.vue';
+import SlotDemo from '3.Slot.vue';
 export default {
  name: 'app',
   components: {
