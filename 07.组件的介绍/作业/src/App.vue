34ffb51704d7388119d75b6a1c98f05bebca87ee
view <view@163.com>
2020-02-19 20:07:40 +0800

    Init:作业初始代码

---
+++ 07.组件的介绍/作业/src/App.vue
@@ -0,0 +1,49 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-02-19 19:50:46
+ * @LastEditTime: 2020-02-19 20:01:49
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue.js终极开发教程\07.组件的介绍\作业\src\App.vue
+ -->
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <header>
+                    <h1>Server Status</h1>
+                </header>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <ul class="list-group">
+                    <li
+                            class="list-group-item"
+                            v-for="index in 5" :key="index">
+                        Server #{{ index }}
+                    </li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <p>Server Details are currently not updated</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12">
+                <footer>
+                    <p>All Servers are managed here</p>
+                </footer>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
---
+++ 07.组件的介绍/作业/src/main.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-19 16:54:37
+ * @LastEditTime: 2020-02-19 20:03:24
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue.js终极开发教程\07.组件的介绍\vue-cli\src\main.js
+ */
+import Vue from 'vue'
+import App from './App.vue'
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
