111a4e06261bc51290a8490a35ac15699e0b9f88
view <view@163.com>
2020-02-19 22:56:32 +0800

    Init:建立一个存在父组件与子组件需要通信的场景,提出如何让父子组件之间进行通信

---
+++ 08组件之间的通信/component_contact/src/components/User.vue
@@ -0,0 +1,52 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-02-19 22:42:51
+ * @LastEditTime: 2020-02-19 22:53:33
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue.js终极开发教程\08组件之间的通信\component_contact\src\components\User.vue
+ -->
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function(){
+            return{
+                name: 'ViewDegree'
+            }
+        },
+        methods:{
+            changeName(){
+                this.name = "ViewBook"
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ 08组件之间的通信/component_contact/src/components/UserDetail.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-02-19 22:42:51
+ * @LastEditTime: 2020-02-19 22:55:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue.js终极开发教程\08组件之间的通信\component_contact\src\components\UserDetail.vue
+ -->
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{name}}</p>
+    </div>
+</template>
+
+<script>
+//如何从父组件User.vue获取父主键修改的值name
+    export default{
+
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
