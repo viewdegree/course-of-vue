18d64a351af577554d3567583301e8198d8cc7b5
view <view@163.com>
2020-02-19 18:26:25 +0800

    Build:用webpack去写用上次提交html用组件写的效果

--- 07.组件的介绍/vue-cli/src/App.vue
+++ 07.组件的介绍/vue-cli/src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-02-19 15:55:02
- * @LastEditTime: 2020-02-19 16:33:16
+ * @LastEditTime: 2020-02-19 18:24:08
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue.js终极开发教程\06.使用webpack和Vue命令进入真正的开发流程\vue-cli\src\App.vue
@@ -9,12 +9,21 @@
 <!-- vue类型的文件只是模板,是用来设置的,真正使用vue文件是main.js -->
 <!-- 只要有模板就能正常工作 -->
 <template>
-  <h1>hello world</h1>
+  <p>Server Status: {{ status }} (<button @click="changeStatus">Change</button>)</p>
 </template>
 <!-- 导出的对象与Vue实例还是构造实例用到的那个对象都是一样的使用,属性也是一样的 -->
 <!-- 导出的目的是把业务逻辑添加到模板上 -->
 <script>
 export default {
-  
+  data:function(){
+    return{
+      status: 'Critical2'
+    }
+  },
+  methods:{
+    changeStatus: function(){
+        this.status = 'Normal'
+    }
+  }
 }
 </script>
--- 07.组件的介绍/vue-cli/src/main.js
+++ 07.组件的介绍/vue-cli/src/main.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-19 16:54:37
+ * @LastEditTime: 2020-02-19 18:19:35
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue.js终极开发教程\07.组件的介绍\vue-cli\src\main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({
   el: '#app',
   render: h => h(App)
+  // render是template属性的替代品但这里是经过webpack渲染编译后的js代码,不再是template属性值的字符串,所以render比template更适合,这是写模板的最好的方法
 })
