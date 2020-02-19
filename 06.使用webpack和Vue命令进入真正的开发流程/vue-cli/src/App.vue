7f7870830faedbbf176bdd5bc8a45a3275ab64a4
view <view@163.com>
2020-02-19 16:35:35 +0800

    Build:vue文件的导出对象

--- 06.使用webpack和Vue命令进入真正的开发流程/vue-cli/src/App.vue
+++ 06.使用webpack和Vue命令进入真正的开发流程/vue-cli/src/App.vue
@@ -1,12 +1,20 @@
 <!--
  * @Author: your name
  * @Date: 2020-02-19 15:55:02
- * @LastEditTime: 2020-02-19 16:20:41
+ * @LastEditTime: 2020-02-19 16:33:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue.js终极开发教程\06.使用webpack和Vue命令进入真正的开发流程\vue-cli\src\App.vue
  -->
+<!-- vue类型的文件只是模板,是用来设置的,真正使用vue文件是main.js -->
 <!-- 只要有模板就能正常工作 -->
 <template>
   <h1>hello world</h1>
 </template>
+<!-- 导出的对象与Vue实例还是构造实例用到的那个对象都是一样的使用,属性也是一样的 -->
+<!-- 导出的目的是把业务逻辑添加到模板上 -->
+<script>
+export default {
+  
+}
+</script>
