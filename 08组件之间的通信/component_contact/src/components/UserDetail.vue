8f06a7765180098a5f8a951f12cefe6949ae17fd
view <view@163.com>
2020-02-19 23:18:46 +0800

    Build:子组件中访问props中的属性和访问data属性一样

--- 08组件之间的通信/component_contact/src/components/UserDetail.vue
+++ 08组件之间的通信/component_contact/src/components/UserDetail.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-02-19 22:42:51
- * @LastEditTime: 2020-02-19 23:04:36
+ * @LastEditTime: 2020-02-19 23:17:49
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue.js终极开发教程\08组件之间的通信\component_contact\src\components\UserDetail.vue
@@ -10,15 +10,21 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name: {{name1}}</p>
+        <p>User Name: {{switchName()}}</p>
     </div>
 </template>
 
 <script>
 //如何从父组件User.vue获取父主键修改的值name
 // props相当于属性data,但是是没有值的,对应父组件写在子组件的name属性中的传
+// 访问props内设置的属性可以像访问属性一样用this访问
     export default{
-        props:['name1']
+        props:['name1'],
+        methods:{
+            switchName(){
+                return this.name1.split("").reverse().join("");
+            }
+        }
     }
 </script>
 
