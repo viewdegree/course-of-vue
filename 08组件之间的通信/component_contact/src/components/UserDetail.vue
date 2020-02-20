2657c18b7353d2fe8b13bb95ae87b5b5d439910a
view <view@163.com>
2020-02-20 11:09:54 +0800

    Build:使用事件总线来通信

--- 08组件之间的通信/component_contact/src/components/UserDetail.vue
+++ 08组件之间的通信/component_contact/src/components/UserDetail.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name   
  * @Date: 2020-02-19 22:42:51
- * @LastEditTime: 2020-02-20 10:03:32
+ * @LastEditTime: 2020-02-20 11:08:25
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue.js终极开发教程\08组件之间的通信\component_contact\src\components\UserDetail.vue
@@ -11,7 +11,7 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{switchName()}}</p>
-        <p>User Age: {{userAge}}</p>
+        <p>User Age: {{MyAge}}</p>
         <button @click="resetNameFn()">Reset Name By self event</button>
         <!-- 采用回调函数 -->
         <button @click="resetFn()">Reset Name By CallBAck Fuction</button>
@@ -25,6 +25,7 @@
 //如何从父组件User.vue获取父主键修改的值name
 // props相当于属性data,但是是没有值的,对应父组件写在子组件的name属性中的传
 // 访问props内设置的属性可以像访问属性一样用this访问
+    import {eventBus} from '../main'
     export default{
         //验证从父组件传入的数据为数字或者其他数据类型造成调用switchName方法的时候出错,里可以限制name1的数据类型 
         //类型有  String Array Boolean Number Object 
@@ -39,6 +40,11 @@
         },
         //因为子级不能直接修改父级的传入,这样不符合数据流就是reset方法不能直接 写
         // this.myName = 'Max';  this.$emit("nameWasReset", this.myName)
+        data(){
+            return{
+                MyAge: this.userAge
+            }
+        },
         methods:{
             switchName(){
                 return this.myName.split("").reverse().join("");
@@ -69,6 +75,11 @@
                     return this.$emit("ageWasReset", val)
                 }
             }
+        },
+        created(){
+            eventBus.$on("changeAge",(age)=>{
+                this.MyAge= age;
+            });
         }
     }
 </script>
--- 08组件之间的通信/component_contact/src/components/UserEdit.vue
+++ 08组件之间的通信/component_contact/src/components/UserEdit.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-02-19 22:42:51
- * @LastEditTime: 2020-02-20 09:45:21
+ * @LastEditTime: 2020-02-20 10:59:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue.js终极开发教程\08组件之间的通信\component_contact\src\components\UserEdit.vue
@@ -10,14 +10,20 @@
     <div class="component">
         <h3>You may edit the User here</h3>
         <p>Edit me!</p>
-        <p>User Age: {{userAge}}</p>
+        <p>User Age: {{myAge}}</p>
         <button @click="editAge">Chang User Name</button>
     </div>
 </template>
 
 <script>
+import {eventBus} from '../main'
 export default{
     props:["userAge"],
+    data(){
+        return {
+            myAge : this.userAge
+        }
+    },
     methods:{
         editAge(){
             this.newAge = 30;
@@ -29,7 +35,9 @@ export default{
                 return this.userAge;
             },
             set(val){
-                 return this.$emit("changeAge", val)
+                this.myAge = val;
+                //  return this.$emit("changeAge", val)
+                eventBus.$emit("changeAge", val)
             }
         }
     }
