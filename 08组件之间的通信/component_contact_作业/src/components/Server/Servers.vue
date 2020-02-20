67d5001cbdf3ea654906504903b37e1988722944
view <view@163.com>
2020-02-20 11:47:51 +0800

    Init:作业项目

---
+++ 08组件之间的通信/component_contact_作业/src/components/Server/Servers.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-02-20 11:42:46
+ * @LastEditTime: 2020-02-20 11:47:21
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vue.js终极开发教程\08组件之间的通信\component_contact_作业\src\components\Server\Servers.vue
+ -->
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                    class="list-group-item"
+                    v-for="index in 5" :key="index">
+                Server #{{ index }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
